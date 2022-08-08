2369d6f64ecc0ec72e2c774c402f7fdbd6f334df
“julian <badjab326@gmail.com>
2022-08-08 13:46:22 -0700

    styling tests

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from "react";
+import { StyleSheet, SafeAreaView, Button, Alert, Platform, StatusBar, View, Dimensions } from 'react-native';
+
 
 export default function App() {
-  console.log("App Executed");
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello Mobile Development</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+
+      <View 
+        style={{
+          backgroundColor: "blue",
+          flex: 1,
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <View 
+          style={{
+            backgroundColor: "yellow",
+            width: 100,
+            height: 300,
+            alignSelf: "flex-start"
+        }}/>
+        <View 
+          style={{
+            backgroundColor: "red",
+            width: 100,
+            height: 200
+        }}/>
+        <View 
+          style={{
+            backgroundColor: "green",
+            width: 100,
+            height: 100
+        }}/>
+
+      </View>
   );
 }
+
+const containerStyle = {backgroundColor: "orange"}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     alignItems: 'center',
     justifyContent: 'center',
   },
