88c101a7acbb62d8c5a9e320bdc756303127d34e
“julian <badjab326@gmail.com>
2022-08-10 08:40:14 -0700

    home screen structure established

--- App.js
+++ App.js
@@ -1,51 +1,8 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Button, Alert, Platform, StatusBar, View, Dimensions } from 'react-native';
+import { View } from 'react-native';
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 
 export default function App() {
-  return (
-
-      <View 
-        style={{
-          backgroundColor: "blue",
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        <View 
-          style={{
-            backgroundColor: "yellow",
-            width: 100,
-            height: 300,
-            alignSelf: "flex-start"
-        }}/>
-        <View 
-          style={{
-            backgroundColor: "red",
-            width: 100,
-            height: 200
-        }}/>
-        <View 
-          style={{
-            backgroundColor: "green",
-            width: 100,
-            height: 100
-        }}/>
-
-      </View>
-  );
+  return <WelcomeScreen /> 
 }
-
-const containerStyle = {backgroundColor: "orange"}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
