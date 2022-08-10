74b1e24f7fdd17c173e92331785966368096f14a
“julian <badjab326@gmail.com>
2022-08-10 12:45:56 -0700

    home screen structure finished, ViewImageScreen created

---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Image, StyleSheet } from 'react-native'
+
+function ViewImageScreen(props) {
+    return (
+        <Image style={styles.image} source={require("../assets/chair.jpg")} />
+    )
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: "100%",
+        height: "100%",
+    }
+})
+
+export default ViewImageScreen
